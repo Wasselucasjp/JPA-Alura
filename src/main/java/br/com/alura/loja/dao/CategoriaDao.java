ae5c41ecd4b605099e5f13b4c252747aac3e9296
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-16 17:57:30 -0300

    remover categoriadao

--- src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -7,13 +7,17 @@ import javax.persistence.EntityManager;
 public class CategoriaDao {
     private EntityManager entityManager;
 
-    public CategoriaDao(EntityManager entityManager){
+    public CategoriaDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar(Categoria categoria){
+    public void cadastrar(Categoria categoria) {
         this.entityManager.persist(categoria);
     }
 
-    public void atualizar(Categoria categoria){this.entityManager.merge(categoria);}
+    public void atualizar(Categoria categoria) {
+        this.entityManager.merge(categoria);
+    }
+
+    public void remover(Categoria categoria) { categoria = entityManager.merge(categoria);  this.entityManager.remove(categoria);}
 }
